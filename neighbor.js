53af80f3c20dc708ccfaaf1013fc16ba346bb420
moldir <moldirshb@gmail.com>
2022-11-18 17:15:01 -0600

    added some changes

--- neighbor.js
+++ neighbor.js
@@ -1,15 +1,16 @@
 
 let randomBtn =document.querySelector('#random')
-// let text = document.querySelector('#text')
+let text =  document.getElementById("text")
 
 let arr = ['Asian', 'Cidercade Austin', 'The Melting Pot', 'Roaring Fork','Barley Swine']
 
-function getRandom(){
-    let random = Math.floor(Math.random() * arr.length)
-    document.getElementById("text").innerHTML += arr[random]
-    return random
+
+
+randomBtn.addEventListener('click',  getRandom=()=>{
     
+    let random = Math.floor(Math.random() * arr.length)
+    text.innerHTML = arr[random]
+   
+    return random
+})
 
-}
-
-randomBtn.addEventListener('click', getRandom)
