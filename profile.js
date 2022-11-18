753431c2b156e4170ba3a70cb0580fd8ba8809dc
moldir <moldirshb@gmail.com>
2022-11-18 14:15:55 -0600

    part4

---
+++ profile.js
@@ -0,0 +1,12 @@
+
+document.querySelector('#color').addEventListener('click', ()=>{
+    alert('My favorite colors: Green, Purple and White')
+})
+
+document.querySelector('#place').addEventListener('click', ()=>{
+    alert('Paris, New York')
+})
+
+document.querySelector('#ritual').addEventListener('click', ()=>{
+    alert('Listening music while coding')
+})
